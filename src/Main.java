47ae7c66de51450c8273e5c247a4cd0237d06bda
Georg Wölflein <georgw7777@gmail.com>
2017-10-28 10:02:29 +0100

    Modify main to accept argument to run as server for testing

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
+import com.guts.michael.connection.Server;
+
 public class Main {
 
     public static void main(String[] args) {
-        new Main();
-    }
-
-    public Main() {
-        ConnectionView.getConnectionView();
+        if (args.length == 1 && args[0].equals("server")) {
+            new Server().start();
+        } else {
+            ConnectionView.getConnectionView();
+        }
     }
 
 }
