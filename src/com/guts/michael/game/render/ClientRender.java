a51478383e3b58505676af925594918606076d14
donald108 <noreply@donald108.com>
2017-10-28 17:22:57 +0100

    Added rendering

--- src/com/guts/michael/game/render/ClientRender.java
+++ src/com/guts/michael/game/render/ClientRender.java
@@ -1,5 +1,6 @@
 package com.guts.michael.game.render;
 
+import com.guts.michael.game.Game;
 import com.guts.michael.game.SpriteLoader;
 
 import javax.swing.*;
@@ -11,13 +12,43 @@ public class ClientRender extends JPanel {
 
     private int c;
     private SpriteLoader loader;
+    private Game game;
 
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    //Sprite is 32*32
+    
     @Override
     protected void paintComponent(Graphics g) {
         if(c != 0) {
             try {
                 loader = new SpriteLoader(32, 32, 8, 8);
-                g.drawImage(loader.getSprites(0), 100, 100, null);
+                for(int i = 0; i < game.getMap().getTiles()[0].length; i++) {
+                    for(int j = 0; j < game.getMap().getTiles().length; i++) {
+                        if((int)(game.getPlayer().getX()/32) == i && (int)(game.getPlayer().getY()/32) == j) {
+                            switch (game.getMap().getTiles()[i][j].getType()) {
+                                case WALL:
+                                    //0
+                                    g.drawImage(loader.getSprites(0), i * 32, j * 32, null);
+                                    break;
+                                case SPACE:
+                                    //1
+                                    g.drawImage(loader.getSprites(1), i * 32, j * 32, null);
+                                    break;
+                                case SAND:
+                                    //1 (for now)
+                                    g.drawImage(loader.getSprites(1), i * 32, j * 32, null);
+                                    break;
+                                case GRASS:
+                                    //2
+                                    g.drawImage(loader.getSprites(2), i * 32, j * 32, null);
+                                    break;
+                            }
+                        }
+                    }
+                }
             } catch (IOException e) {
                 System.err.println("Spritesheet not found!!");
                 System.exit(1);
--- src/com/guts/michael/game/render/ServerRender.java
+++ src/com/guts/michael/game/render/ServerRender.java
@@ -1,5 +1,8 @@
 package com.guts.michael.game.render;
 
+import com.guts.michael.game.Game;
+import com.guts.michael.game.IMap;
+import com.guts.michael.game.Map;
 import com.guts.michael.game.SpriteLoader;
 
 import javax.swing.*;
@@ -12,13 +15,39 @@ public class ServerRender extends JPanel {
 
     private int c;
     private SpriteLoader loader;
+    private Game game;
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
 
     @Override
     protected void paintComponent(Graphics g) {
         if(c != 0) {
             try {
                 loader = new SpriteLoader(32, 32, 8, 8);
-                g.drawImage(loader.getSprites(1), 100, 100, null);
+                for(int i = 0; i < game.getMap().getTiles()[0].length; i++) {
+                    for(int j = 0; j < game.getMap().getTiles().length; i++) {
+                        switch (game.getMap().getTiles()[i][j].getType()) {
+                            case WALL:
+                                //0
+                                g.drawImage(loader.getSprites(0), i*32, j*32, null);
+                                break;
+                            case SPACE:
+                                //1
+                                g.drawImage(loader.getSprites(1), i*32, j*32, null);
+                                break;
+                            case SAND:
+                                //1 (for now)
+                                g.drawImage(loader.getSprites(1), i*32, j*32, null);
+                                break;
+                            case GRASS:
+                                //2
+                                g.drawImage(loader.getSprites(2), i*32, j*32, null);
+                                break;
+                        }
+                    }
+                }
             } catch (IOException e) {
                 System.err.println("Spritesheet not found!!");
                 System.exit(1);
