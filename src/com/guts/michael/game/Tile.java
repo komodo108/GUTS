be0441df5831e60ded8cf16597b52d205e64cb74
Georg Wölflein <georgw7777@gmail.com>
2017-10-28 10:02:03 +0100

    Add tile types

--- src/com/guts/michael/game/Tile.java
+++ src/com/guts/michael/game/Tile.java
@@ -2,8 +2,14 @@ package com.guts.michael.game;
 
 public class Tile implements ITile {
 
+    private TileType type;
+
+    public Tile(TileType type) {
+        this.type = type;
+    }
+
     @Override
     public TileType getType() {
-        return null;
+        return type;
     }
 }
--- src/com/guts/michael/game/TileType.java
+++ src/com/guts/michael/game/TileType.java
@@ -1,5 +1,24 @@
 package com.guts.michael.game;
 
 public enum TileType {
+    SPACE(".");
 
+    private String representation;
+
+    TileType(String representation) {
+        this.representation = representation;
+    }
+
+    public String getStringRepresentation() {
+        return representation;
+    }
+
+    public static TileType fromStringRepresentation(String representation) {
+        for (TileType t : TileType.values()) {
+            if (t.getStringRepresentation().equals(representation)) {
+                return t;
+            }
+        }
+        return null;
+    }
 }
