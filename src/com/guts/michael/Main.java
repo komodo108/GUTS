505bf4856a8b4c00f89c173ad418502feaa4632e
Georg Wölflein <georgw7777@gmail.com>
2017-10-29 11:18:20 +0000

    Change port pack

--- src/com/guts/michael/Main.java
+++ src/com/guts/michael/Main.java
@@ -4,7 +4,7 @@ import com.guts.michael.views.SelectorView;
 
 public class Main {
 
-    public static final int PORT = 80;
+    public static final int PORT = 25678;
 
     public static void main(String[] args) {
         SelectorView.getSelector();
